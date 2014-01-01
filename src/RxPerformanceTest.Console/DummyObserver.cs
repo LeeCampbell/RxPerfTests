eeb771aef3e10daaf7c82cf763023c74e7dbd3fc
Lee Campbell <Lee.ryan.campbell@gmail.com>
2014-01-01 00:24:07 +0000

    Initial commit

---
+++ src/RxPerformanceTest.Console/DummyObserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RxPerformanceTest.Console
+{
+    internal sealed class DummyObserver : IObserver<int>
+    {
+        public int OnNextCount { get; set; }
+        public int OnErrorCount { get; set; }
+        public int OnCompletedCount { get; set; }
+
+        void IObserver<int>.OnCompleted()
+        {
+            OnCompletedCount++;
+        }
+
+        void IObserver<int>.OnError(Exception error)
+        {
+            OnErrorCount++;
+        }
+
+        void IObserver<int>.OnNext(int value)
+        {
+            OnNextCount++;
+        }
+    }
+}
---
+++ src/RxPerformanceTest.Console/SubjectResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace RxPerformanceTest.Console
+{
+    public sealed class SubjectResolver
+    {
+        private readonly string _basePath;
+
+        public SubjectResolver(string basePath)
+        {
+            _basePath = basePath;
+            AppDomain.CurrentDomain.AssemblyResolve += LoadFromSameFolder;
+        }
+
+        public object GetInstance(string assemblyName, string subjectTypeName, params object[] args)
+        {
+            var assemblyFullPath = Path.Combine(_basePath, assemblyName);
+            var ass = Assembly.LoadFile(assemblyFullPath);
+
+            var subjectType = ass.GetType(subjectTypeName);
+            var itemType = typeof(int);
+            return Activator.CreateInstance(subjectType.MakeGenericType(itemType), args);
+        }
+
+        private Assembly LoadFromSameFolder(object sender, ResolveEventArgs args)
+        {
+            string assemblyPath = Path.Combine(_basePath, new AssemblyName(args.Name).Name + ".dll");
+            if (File.Exists(assemblyPath) == false) return null;
+            var assembly = Assembly.LoadFrom(assemblyPath);
+            return assembly;
+        }
+    }
+}
