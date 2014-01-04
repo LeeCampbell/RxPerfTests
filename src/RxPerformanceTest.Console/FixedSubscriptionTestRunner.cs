dbf7acd4b50e85670a0c4d6d5c6e5960636d8c97
Lee Campbell <Lee.ryan.campbell@gmail.com>
2014-01-04 06:58:21 +0000

    Writing output as grids for charting

--- src/RxPerformanceTest.Console/FixedSubscriptionTestRunner.cs
+++ src/RxPerformanceTest.Console/FixedSubscriptionTestRunner.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
 
 namespace RxPerformanceTest.Console
 {
-    //TODO: OUtput the results in to grids, ready to be turned into a surface chart. -LC
     public class FixedSubscriptionTestRunner
     {
         private readonly string _testPath;
@@ -26,9 +27,10 @@ namespace RxPerformanceTest.Console
                 System.Console.Write(".");
             }
             System.Console.WriteLine(" - Done.");
+            System.Console.WriteLine();
 
             //Actual run.
-            System.Console.WriteLine("Version         TestName             Subscriptions Messages  GC's Seconds msg/sec");
+
 
             RunForVariation("Replay()              ", true);
             RunForVariation("Replay(1)             ", true, 1);
@@ -46,35 +48,130 @@ namespace RxPerformanceTest.Console
             {
                 var subject = ctor();
                 Run((IObservable<int>)subject, (IObserver<int>)subject, 10000, 1);
-                Run((IObservable<int>) subject, (IObserver<int>) subject, 10000, 4);
+                Run((IObservable<int>)subject, (IObserver<int>)subject, 10000, 4);
             }
             else
             {
-                for (int i = 0; i < 5; i++)
+                var cols = 5;
+                var rows = 4;
+                var rowOffset = 3;
+                var columnHeaders = new string[cols];
+                var rowHeaders = new string[rows];
+                var results = new TestResult[cols, rows];
+
+                for (int x = 0; x < cols; x++)
                 {
-                    int subscriptions = (int) Math.Pow(2, i);
-                    for (int j = 3; j < 7; j++)
+                    int subscriptions = (int)Math.Pow(2, x);
+                    columnHeaders[x] = subscriptions.ToString();
+
+                    for (int y = 0; y < rows; y++)
                     {
-                        int onNextCount = (int) Math.Pow(10, j);
+                        var j = y + rowOffset;
+                        int messages = (int)Math.Pow(10, j);
+                        rowHeaders[y] = messages.ToString();
                         var subject = ctor();
-                        var result = Run((IObservable<int>) subject, (IObserver<int>) subject, onNextCount,
-                                         subscriptions);
-                            WriteResults(subject, testName, subscriptions, onNextCount, result);
+                        var result = Run((IObservable<int>)subject, (IObserver<int>)subject, messages, subscriptions);
+                        results[x, y] = result;
                     }
                 }
+                var version = ctor().GetType().Assembly.GetName().Version.ToString();
+                System.Console.WriteLine("Rx v{0} {1} - Throughput (msg/sec)", version, testName.Trim());
+                //var output = ResultsToCsv("Subscriptions", columnHeaders,
+                //                          "Messages", rowHeaders,
+                //                          (x, y) => (results[x, y].Messages / results[x, y].Elapsed.TotalSeconds).ToString("#"));
+                var output = ResultsToFixedWdith("Subscriptions", columnHeaders,
+                                                 "Messages", rowHeaders,
+                                                 (x, y) => (results[x, y].Messages / results[x, y].Elapsed.TotalSeconds).ToString("#"));
+                System.Console.WriteLine(output);
+                
+                System.Console.WriteLine("Rx v{0} {1} - GCs", version, testName.Trim());
+                output = ResultsToFixedWdith("Subscriptions", columnHeaders,
+                                                 "Messages", rowHeaders,
+                                                 (x, y) => (results[x, y].Gen0Collections).ToString());
+
+
+                System.Console.WriteLine(output);
+                System.Console.WriteLine();
+                System.Console.WriteLine();
             }
+        }
+
+        private static string ResultsToCsv(string columnLabel, string[] columnHeaders, string rowLabel, string[] rowHeaders, Func<int, int, string> valueSelector)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine();
+            sb.Append(",");
+            sb.Append(columnLabel);
+            sb.AppendLine();
+            sb.Append(rowLabel);
+            foreach (string header in columnHeaders)
+            {
+                sb.AppendFormat(",{0}", header);
+            }
+            sb.AppendLine();
+            for (int y = 0; y < rowHeaders.Length; y++)
+            {
+                sb.Append(rowHeaders[y]);
+                for (int x = 0; x < columnHeaders.Length; x++)
+                {
+                    sb.Append(",");
+                    sb.Append(valueSelector(x, y));
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        private static string ResultsToFixedWdith(string columnLabel, string[] columnHeaders, string rowLabel, string[] rowHeaders, Func<int, int, string> valueSelector)
+        {
+            var maxValueLength = columnHeaders.Max(h => h.Length);
+            var values = new string[columnHeaders.Length, rowHeaders.Length];
+            for (int y = 0; y < rowHeaders.Length; y++)
+            {
+                for (int x = 0; x < columnHeaders.Length; x++)
+                {
+                    var value = valueSelector(x, y);
+                    values[x, y] = value;
+                    if (value.Length > maxValueLength) maxValueLength = value.Length;
+                }
+            }
+
+            var colWidth = maxValueLength + 1;
+            var labelWidth = rowHeaders.Concat(new[] { rowLabel }).Max(h => h.Length) + 1;
+
+            var sb = new StringBuilder();
+            sb.Append("".PadRight(labelWidth));
+            sb.Append(columnLabel);
+            sb.AppendLine();
+            sb.Append(rowLabel.PadLeft(labelWidth));
+            foreach (string header in columnHeaders)
+            {
+                sb.Append(header.PadLeft(colWidth));
+            }
+            sb.AppendLine();
+            for (int y = 0; y < rowHeaders.Length; y++)
+            {
+                sb.Append(rowHeaders[y].PadLeft(labelWidth));
+                for (int x = 0; x < columnHeaders.Length; x++)
+                {
+
+                    sb.Append(valueSelector(x, y).PadLeft(colWidth));
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
         }
 
         private static void WriteResults(object subject, string testName, int subscriptions, int onNextCount, TestResult result)
         {
-            System.Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", 
-                subject.GetType().Assembly.GetName().Version.ToString().PadRight(15), 
-                testName, 
-                subscriptions.ToString().PadLeft(11), 
-                onNextCount.ToString().PadLeft(8), 
-                result.Gen0Collections.ToString().PadLeft(5), 
-                result.Elapsed.TotalSeconds.ToString("000.000"), 
-                (onNextCount/result.Elapsed.TotalSeconds).ToString("000000000.0"));
+            System.Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}",
+                subject.GetType().Assembly.GetName().Version.ToString().PadRight(15),
+                testName,
+                subscriptions.ToString().PadLeft(11),
+                onNextCount.ToString().PadLeft(8),
+                result.Gen0Collections.ToString().PadLeft(5),
+                result.Elapsed.TotalSeconds.ToString("000.000"),
+                (onNextCount / result.Elapsed.TotalSeconds).ToString("000000000.0"));
         }
 
         public static TestResult Run(IObservable<int> observable, IObserver<int> observer, int onNextCount, int subscriptionCount)
@@ -101,11 +198,13 @@ namespace RxPerformanceTest.Console
             {
                 subscriptions[i].Dispose();
             }
-
+            var gcs = GC.CollectionCount(0) - preRunGen0AllocationCount;
             return new TestResult
                 {
+                    Subscriptions = subscriptionCount,
+                    Messages = onNextCount,
                     Elapsed = timer.Elapsed,
-                    Gen0Collections = GC.CollectionCount(0) - preRunGen0AllocationCount
+                    Gen0Collections = gcs
                 };
         }
     }
--- src/RxPerformanceTest.Console/TestResult.cs
+++ src/RxPerformanceTest.Console/TestResult.cs
@@ -4,6 +4,8 @@ namespace RxPerformanceTest.Console
 {
     public sealed class TestResult
     {
+        public int Subscriptions { get; set; }
+        public int Messages { get; set; }
         public int Gen0Collections { get; set; }
         public TimeSpan Elapsed { get; set; }
     }
